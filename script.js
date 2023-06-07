74cd7e41c4bcb04161ad18078ac0e2995ac30e11
sriram3640 <104811997+sriram3640@users.noreply.github.com>
2023-06-07 10:23:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,138 @@
-//image popup
-document.addEventListener('DOMContentLoaded', function() {
-  var images = document.getElementsByClassName('portfolio-image');
-  var popup = document.getElementById('image-popup');
-  var popupImage = document.getElementById('popup-image');
-  
-  function openPopup(imageSrc) {
-    popupImage.src = imageSrc;
-    popup.style.display = 'block';
-  }
-  
-  function closePopup() {
-    popup.style.display = 'none';
-    popupImage.src = '';
-  }
-  
-  function handleImageClick(event) {
-    event.stopPropagation();
-    var imageSrc = this.src;
-    openPopup(imageSrc);
-  }
-  
-  function handleDocumentClick(event) {
-    if (!popup.contains(event.target)) {
-      closePopup();
-    }
-  }
+var property = new Array();
+var unit = new Array();
+var factor = new Array();
 
-  for (var i = 0; i < images.length; i++) {
-    images[i].addEventListener('click', handleImageClick);
-  }
-  
-  popup.addEventListener('click', closePopup);
+property[0] = "Length";
+unit[0] = new Array("Meter (m)", "Angstrom (A')", "Astronomical unit (AU)", "Caliber (cal)", "Centimeter (cm)", "Kilometer (km)", "Ell", "Em", "Fathom", "Furlong", "Fermi (fm)", "Foot (ft)", "Inch (in)", "League (int'l)", "League (UK)", "Light year (LY)", "Micrometer (mu-m)", "Mil", "Millimeter (mm)", "Nanometer (nm)", "Mile (int'l nautical)", "Mile (UK nautical)", "Mile (US nautical)", "Mile (US statute)", "Parsec", "Pica (printer)", "Picometer (pm)", "Point (pt)", "Rod", "Yard (yd)");
+factor[0] = new Array(1, 1E-10, 1.49598E11, .000254, .01, 1000, 1.143, 4.2323E-03, 1.8288, 201.168, 1E-15, .3048, .0254, 5556, 5556, 9.46055E+15, .000001, .0000254, .001, 1E-9, 1852, 1853.184, 1852, 1609.344, 3.08374E+16, 4.217518E-03, 1E-12, .0003514598, 5.0292, .9144);
 
-  document.addEventListener('click', handleDocumentClick);
-});
+property[1] = "Mass";
+unit[1] = new Array("Kilogram (kgr)", "Gram (gr)", "Milligram (mgr)", "Microgram (mu-gr)", "Carat (metric)(ct)", "Hundredweight (long)", "Hundredweight (short)", "Pound mass (lbm)", "Pound mass (troy)", "Ounce mass (ozm)", "Ounce mass (troy)", "Slug", "Ton (assay)", "Ton (long)", "Ton (short)", "Ton (metric)", "Tonne");
+factor[1] = new Array(1, .001, 1e-6, .000000001, .0002, 50.80235, 45.35924, .4535924, .3732417, .02834952, .03110348, 14.5939, .02916667, 1016.047, 907.1847, 1000, 1000);
 
-//smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
+property[2] = "Time";
+unit[2] = new Array("Second (sec)", "Day (mean solar)", "Day (sidereal)", "Hour (mean solar)", "Hour (sidereal)", "Minute (mean solar)", "Minute (sidereal)", "Month (mean calendar)", "Second (sidereal)", "Year (calendar)", "Year (tropical)", "Year (sidereal)");
+factor[2] = new Array(1, 8.640E4, 86164.09, 3600, 3590.17, 60, 60, 2628000, .9972696, 31536000, 31556930, 31558150);
 
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
+property[3] = "Light";
+unit[3] = new Array("Lumen/sq.meter (Lu/m^2)", "Lumen/sq.centimeter", "Lumen/sq.foot", "Foot-candle (ft-cdl)", "Foot-lambert", "Candela/sq.meter", "Candela/sq.centimeter", "Lux (lux)", "Phot");
+factor[3] = new Array(1, 10000, 10.76391, 10.76391, 10.76391, 3.14159250538575, 31415.9250538576, 1, 10000);
 
-// Toggle navbar on mobile devices
-function toggleNavbar() {
-  let navbar = document.querySelector('.navbar-container');
-  navbar.classList.toggle('responsive');
+property[4] = "Temperature";
+unit[4] = new Array("Degrees Celsius ('C)", "Degrees Fahrenheit ('F)", "Degrees Kelvin ('K)", "Degrees Rankine ('R)");
+factor[4] = new Array(1, 0.555555555555, 1, 0.555555555555);
+add_temp = new Array(0, -32, -273.15, -491.67);
+
+property[5] = "Electricity";
+unit[5] = new Array("Coulomb (Cb)", "Abcoulomb", "Ampere hour (A hr)", "Faraday (F)", "Statcoulomb", "Millifaraday (mF)", "Microfaraday (mu-F)", "Picofaraday (pF)");
+factor[5] = new Array(1, 10, 3600, 96521.8999999997, .000000000333564, 96.5219, 9.65219E-02, 9.65219E-05);
+
+property[6] = "Area";
+unit[6] = new Array("Square meter (m^2)", "Acre (acre)", "Are", "Barn (barn)", "Hectare", "Rood", "Square centimeter", "Square kilometer", "Circular mil", "Square foot (ft^2)", "Square inch (in^2)", "Square mile (mi^2)", "Square yard (yd^2)");
+factor[6] = new Array(1, 4046.856, 100, 1E-28, 10000, 1011.71413184285, .0001, 1000000, 5.067075E-10, 9.290304E-02, 6.4516E-04, 2589988, .8361274);
+
+
+property[7] = "Velocity & Speed";
+unit[7] = new Array("Meter/second (m/sec)", "Foot/minute (ft/min)", "Foot/second (ft/sec)", "Kilometer/hour (kph)", "Knot (int'l)", "Mile (US)/hour (mph)", "Mile (nautical)/hour", "Mile (US)/minute", "Mile (US)/second", "Speed of light (c)", "Mach (STP)(a)");
+factor[7] = new Array(1, 5.08E-03, .3048, .2777778, .5144444, .44707, .514444, 26.8224, 1609.344, 299792458, 340.0068750);
+
+
+property[8] = "Acceleration";
+unit[8] = new Array("Meter/sq.sec (m/sec^2)", "Foot/sq.sec (ft/sec^2)", "G (g)", "Galileo (gal)", "Inch/sq.sec (in/sec^2)");
+factor[8] = new Array(1, .3048, 9.806650, .01, 2.54E-02);
+
+
+property[9] = "Volume & Capacity";
+unit[9] = new Array("Cubic Meter (m^3)", "Cubic centimeter", "Cubic millimeter", "Acre-foot", "Barrel (oil)", "Board foot", "Bushel (US)", "Cup", "Fluid ounce (US)", "Cubic foot", "Gallon (UK)", "Gallon (US,dry)", "Gallon (US,liq)", "Gill (UK)", "Gill (US)", "Cubic inch (in^3)", "Liter (new)", "Liter (old)", "Ounce (UK,fluid)", "Ounce (US,fluid)", "Peck (US)", "Pint (US,dry)", "Pint (US,liq)", "Quart (US,dry)", "Quart (US,liq)", "Stere", "Tablespoon", "Teaspoon", "Ton (register)", "Cubic yard");
+factor[9] = new Array(1, .000001, .000000001, 1233.482, .1589873, .002359737, .03523907, .0002365882, .00002957353, .02831685, .004546087, .004404884, .003785412, .0001420652, .0001182941, .00001638706, .001, .001000028, .00002841305, .00002957353, 8.8097680E-03, .0005506105, 4.7317650E-04, .001101221, 9.46353E-04, 1, .00001478676, .000004928922, 2.831685, .7645549);
+
+property[10] = "Density & Mass capacity";
+unit[10] = new Array("Kilogram/cub.meter", "Grain/galon", "Grams/cm^3 (gr/cc)", "Pound mass/cubic foot", "Pound mass/cubic-inch", "Ounces/gallon (UK,liq)", "Ounces/gallon (US,liq)", "Ounces (mass)/inch", "Pound mass/gal (UK,liq)", "Pound mass/gal (US,liq)", "Slug/cubic foot", "Tons (long,mass)/cub.yard");
+factor[10] = new Array(1, .01711806, 1000, 16.01846, 27679.91, 6.236027, 7.489152, 1729.994, 99.77644, 119.8264, 515.379, 1328.939);
+
+property[11] = "Force";
+unit[11] = new Array("Newton (N)", "Dyne (dy)", "Kilogram force (kgf)", "Kilopond force (kpf)", "Kip (k)", "Ounce force (ozf)", "Pound force (lbf)", "Poundal");
+factor[11] = new Array(1, .00001, 9.806650, 9.806650, 4448.222, .2780139, .4535924, .138255);
+
+property[12] = "Energy";
+unit[12] = new Array("Joule (J)", "BTU (mean)", "BTU (thermochemical)", "Calorie (SI) (cal)", "Calorie (mean)(cal)", "Calorie (thermo)", "Electron volt (eV)", "Erg (erg)", "Foot-pound force", "Foot-poundal", "Horsepower-hour", "Kilocalorie (SI)(kcal)", "Kilocalorie (mean)(kcal)", "Kilowatt-hour (kW hr)", "Ton of TNT", "Volt-coulomb (V Cb)", "Watt-hour (W hr)", "Watt-second (W sec)");
+factor[12] = new Array(1, 1055.87, 1054.35, 4.1868, 4.19002, 4.184, 1.6021E-19, .0000001, 1.355818, 4.214011E-02, 2684077.3, 4186.8, 4190.02, 3600000, 4.2E9, 1, 3600, 1);
+
+property[13] = "Power";
+unit[13] = new Array("Watt (W)", "Kilowatt (kW)", "Megawatt (MW)", "Milliwatt (mW)", "BTU (SI)/hour", "BTU (thermo)/second", "BTU (thermo)/minute", "BTU (thermo)/hour", "Calorie (thermo)/second", "Calorie (thermo)/minute", "Erg/second", "Foot-pound force/hour", "Foot-pound force/minute", "Foot-pound force/second", "Horsepower(550 ft lbf/s)", "Horsepower (electric)", "Horsepower (boiler)", "Horsepower (metric)", "Horsepower (UK)", "Kilocalorie (thermo)/min", "Kilocalorie (thermo)/sec");
+factor[13] = new Array(1, 1000, 1000000, .001, .2930667, 1054.35, 17.5725, .2928751, 4.184, 6.973333E-02, .0000001, .0003766161, .02259697, 1.355818, 745.7, 746, 9809.5, 735.499, 745.7, 69.7333, 4184);
+
+property[14] = "Pressure & Stress";
+unit[14] = new Array("Newton/sq.meter", "Atmosphere (normal)", "Atmosphere (techinical)", "Bar", "Centimeter mercury(cmHg)", "Centimeter water (4'C)", "Decibar", "Kgr force/sq.centimeter", "Kgr force/sq.meter", "Kip/square inch", "Millibar", "Millimeter mercury(mmHg)", "Pascal (Pa)", "Kilopascal (kPa)", "Megapascal (Mpa)", "Poundal/sq.foot", "Pound-force/sq.foot", "Pound-force/sq.inch (psi)", "Torr (mmHg,0'C)");
+factor[14] = new Array(1, 101325, 98066.5, 100000, 1333.22, 98.0638, 10000, 98066.5, 9.80665, 6894757, 100, 133.3224, 1, 1000, 1000000, 47.88026, 47.88026, 6894.757, 133.322);
+
+property[15] = "Torque";
+unit[15] = new Array("Newton-meter (N m)", "Dyne-centimeter(dy cm)", "Kgrf-meter (kgf m)", "lbf-inch (lbf in)", "lbf-foot (lbf ft)");
+factor[15] = new Array(1, .0000001, 9.806650, .1129848, 1.355818);
+
+function main_property(propMenu, unitMenu) {
+  var i;
+  i = propMenu.selectedIndex;
+  Arrayfilling(unitMenu, unit[i]);
 }
+
+function Arrayfilling(myMenu, myArray) {
+  var i;
+  myMenu.length = myArray.length;
+  for (i = 0; i < myArray.length; i++) {
+    myMenu.options[i].text = myArray[i];
+  }
+}
+
+function find_value(sform, tform) {
+  var svalue = sform.input_unit.value;
+  svalue = parseFloat(svalue);
+  if (!isNaN(svalue) || svalue == 0) {
+    sform.input_unit.value = svalue;
+    ConvertFromTo(sform, tform);
+  }
+}
+
+function ConvertFromTo(sform, tform) {
+  var pindex;
+  var sindex;
+  var sfactor;
+  var tindex;
+  var tfactor;
+  var answer;
+  pindex = document.choose_property.properties.selectedIndex;
+  sindex = sform.values.selectedIndex;
+  sfactor = factor[pindex][sindex];
+  tindex = tform.values.selectedIndex;
+  tfactor = factor[pindex][tindex];
+  answer = sform.input_unit.value;
+  if (property[pindex] == "Temperature") {
+    answer = parseFloat(answer) + add_temp[sindex];
+  }
+  answer = answer * sfactor;
+  answer = answer / tfactor;
+  if (property[pindex] == "Temperature") {
+    answer = parseFloat(answer) - add_temp[tindex];
+  }
+  tform.input_unit.value = answer;
+}
+
+window.onload = function(e) {
+  Arrayfilling(document.choose_property.properties, property);
+  main_property(document.choose_property.properties, document.firstform.values);
+  main_property(document.choose_property.properties, document.secondform.values)
+}
+
+document.getElementByClass('numb').addEventListener('keydown', function(e) {
+  var key = e.keyCode ? e.keyCode : e.which;
+
+  if (!([8, 9, 13, 27, 46, 110, 190].indexOf(key) !== -1 ||
+      (key == 65 && (e.ctrlKey || e.metaKey)) ||
+      (key == 67 && (e.ctrlKey || e.metaKey)) || 
+      (key == 86 && (e.ctrlKey || e.metaKey)) || 
+      (key >= 35 && key <= 40) || 
+      (key >= 48 && key <= 57 && !(e.shiftKey || e.altKey)) || 
+      (key >= 96 && key <= 105) 
+      (key == 190) 
+    )) e.preventDefault();
+});
